84ae45610d950430176ea0fca032b55308e8cc72
S4URABH <saurabhdaware99@gmail.com>
2019-07-05 17:51:52 +0530

    CSS changes

--- src/webview.ts
+++ src/webview.ts
@@ -1,3 +1,6 @@
+// I've only included HTML in this and CSS and JS files are linked externally
+// If you are here to contribute to the UI just edit the files in /ui folder and test it from index.html and then create a pull request
+// I will take the files from ui folder and will host it over cdn
 const html = `
 <!DOCTYPE html>
 <html>
